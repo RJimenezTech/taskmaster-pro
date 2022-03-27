7d464dfdfb6b3db3b54a2557c521f974ce5b7d0d
Robert Jimenez <robertjimenez86@gmail.com>
2022-03-27 11:50:28 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,15 +82,23 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event, ui) {
     console.log(ui);
+    $(this).addClass("dropover");
+    $("bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event, ui) {
     console.log(ui);
+    $(this).addClass("dropover");
+    $("bottom-trash").addClass("bottom-trash-drag");
   },
   over: function(event) {
     console.log(event);
+    $(this).addClass("dropover-active");
+    $("bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event) {
     console.log(event);
+    $(this).addClass("dropover-active");
+    $("bottom-trash").addClass("bottom-trash-active");
   },
   update: function() {
     var tempArr = [];
@@ -161,7 +169,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -286,6 +294,11 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  })
+}, (1000*60) * 30);
 // load tasks for the first time
 loadTasks();
 
